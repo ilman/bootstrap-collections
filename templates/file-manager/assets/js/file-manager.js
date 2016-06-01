b68181fe234ab68d1b1905b9d2c3c93249d897ba
Ilman Maulana <ilman.maulana@gmail.com>
2016-06-02 00:41:37 +0700

    Add reset and remove selection functions

--- templates/file-manager/assets/js/file-manager.js
+++ templates/file-manager/assets/js/file-manager.js
@@ -2,6 +2,23 @@ jQuery(document).ready(function($){
 
 	'use strict';
 
+	var $file_container = $('#file-manager-app');
+	var $col_details = $file_container.find('#col-file-sidebar');
+	var $col_files = $file_container.find('#col-file-main');
+	var $file_grid = $file_container.find('#file-grid');
+
+	if(typeof window.fileManager == 'undefined'){
+		window.fileManager = {
+			'selected_files': [],
+			'multiple_select': true
+		};
+	}
+
+	/*---template underscorejs---*/
+	window.fileManager.template = function(id){
+		return _.template($('#'+id).html());
+	};
+
 	/*---functions---*/
 	function init(){
 		if(typeof window.fileManager.filtered_list == 'undefined'){
@@ -21,50 +38,28 @@ jQuery(document).ready(function($){
 		if(! newcontent){
 			newcontent = '<li><p class="alert alert-warning" style="margin-right:15px;">no content</p></li>';
 		}
-		window.fileManager.$file_grid.html(newcontent);
+		$file_grid.html(newcontent);
 
 
 		// match height
 		setTimeout(function(){
-			if(window.fileManager.$col_details.height() < window.fileManager.$col_files.height()){
-				window.fileManager.$col_details.css('min-height', window.fileManager.$col_files.height()-30);
+			if($col_details.height() < $col_files.height()){
+				$col_details.css('min-height', $col_files.height()-30);
 			} 
 		}, 1);
 	}
-	/*---end of functions---*/
 
-
-	var $file_manager = $('#file-manager-app');
-
-
-
-	if(typeof window.fileManager == 'undefined'){
-		window.fileManager = {
-			'$col_details': $file_manager.find('#col-file-sidebar'),
-			'$col_files': $file_manager.find('#col-file-main'),
-			'$file_grid': $file_manager.find('#file-grid'),
-			'selected_files': [],
-			'multiple_select': true
-		};
-	}
-
-	// template using underscore.js
-	window.fileManager.template = function(id){
-		return _.template($('#'+id).html());
-	};
 
 	/*---init---*/
 	window.fileManager.filtered_list = window.file_list;
 	init();
 
-	/*---file detail---*/
+	/*---select file---*/
 	
-	$file_manager.find('#file-grid').on('click','.item',function(){
+	$file_grid.on('click','.item',function(){
 
 		var key = $(this).data('key');
 		var data = window.fileManager.filtered_list[key];
-		var tmpl = window.fileManager.template('file_detail');
-		var newcontent = '';
 
 		if(typeof key == 'undefined'){
 			return;
@@ -78,17 +73,45 @@ jQuery(document).ready(function($){
 			window.fileManager.selected_files = [data];
 		}
 
+		showSelectedFiles();
+	});
+
+
+	function showSelectedFiles(){
+		var tmpl = window.fileManager.template('file_detail');
+		var content = '';
+
 		if(window.fileManager.selected_files.length > 1){
+			content = '<p><button type="button" class="btn btn-default btn-block js-reset-selection">Reset</button></p>';
 			tmpl = window.fileManager.template('files_selected');
 		}
 
-		console.log(window.fileManager.selected_files);
-
 		for(var i=0; i<window.fileManager.selected_files.length; i++){
-			newcontent += tmpl(window.fileManager.selected_files[i]);			
+			content += tmpl(window.fileManager.selected_files[i]);			
 		}		
 
-		window.fileManager.$col_details.html(newcontent);
+		$col_details.html(content);
+	}
+
+	/*---reset selection---*/
+	
+	$col_details.on('click','.js-reset-selection',function(){
+
+		window.fileManager.selected_files = [];
+
+		showSelectedFiles();
+	});
+
+	/*---remove selection---*/
+	
+	$col_details.on('click','.js-unselect',function(){
+		var $this = $(this);
+
+		window.fileManager.selected_files = _.filter(window.fileManager.selected_files, function(item){
+			return item.key != $this.data('key');
+		});
+
+		showSelectedFiles();
 	});
 
 
@@ -97,7 +120,7 @@ jQuery(document).ready(function($){
 	(function(){ 
 
 
-		var $navbar = $file_manager.find('#navbar');
+		var $navbar = $file_container.find('#navbar');
 		var $filter = $navbar.find('#filter');
 		var $type = $navbar.find('#filter-type');
 		var value = {};
@@ -105,7 +128,7 @@ jQuery(document).ready(function($){
 
 		function filter(){
 
-			window.fileManager.filtered_list =_.filter(window.file_list, function(item){
+			window.fileManager.filtered_list = _.filter(window.file_list, function(item){
 				var is_true = true;
 
 				if(typeof value.type != 'undefined' && value.type){
@@ -127,8 +150,6 @@ jQuery(document).ready(function($){
 			e.preventDefault();
 
 			value.filter = $(this).val();
-
-			console.log(value);
 			
 			filter();
 
@@ -146,12 +167,60 @@ jQuery(document).ready(function($){
 			e.preventDefault();
 
 			value.type = $(this).val();
-
-			console.log(value);
 			
 			filter();
 		});
 
 
 	})();
+
+
+	/*---apply link---*/
+
+	window.fileManager.applyLink = function(field_id)
+	{
+		if($('#is-iframe').val() == 1){
+			var windowParent = window.parent;
+		}
+		else{
+			var windowParent = window.opener;
+		}
+
+		// var path = $('#cur_dir').val();
+		// path = path.replace('\\', '/');
+		// var subdir = $('#subdir').val();
+		// subdir = subdir.replace('\\', '/');
+		// var base_url = $('#base_url').val();
+		var files = window.fileManager.selected_files;
+
+		// console.log('applyLink', field_id, files, url);
+
+		// if(field_id){
+		// 	var target = $('#' + external, windowParent.document);
+		// 	target.val(url).trigger('change');
+		// }
+
+		if (typeof windowParent.applyLinkCallback == 'function'){
+			windowParent.applyLinkCallback(files);
+		}
+		closeWindow();
+	}
+
+
+
+	function closeWindow(){
+
+		if($('#is-iframe').val() == 1){
+			var windowParent = window.parent;
+		}
+		else{
+			window.close();
+		}
+	}
+
+	$('#apply-link').on('click', function(){
+		window.fileManager.applyLink();
+	});
+
+
 });
